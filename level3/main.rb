3c10bb578e4a3fc07b0384f0128a023ea9ea8f73
Thomas Sertorio <thomas.sertorio@gmail.com>
2025-05-20 22:51:49 +0200

    [Level 3] Commission

---
+++ level3/main.rb
@@ -0,0 +1,26 @@
+require "json"
+require_relative "lib/car"
+require_relative "lib/rental"
+require_relative "lib/commission"
+
+# Load input
+input = JSON.parse(File.read("data/input.json"), symbolize_names: true)
+
+# Build cars lookup
+cars = input[:cars].each_with_object({}) do |car_data, hash|
+  car = Car.new(**car_data)
+  hash[car.id] = car
+end
+
+# Build rentals lookup
+rentals = input[:rentals].each_with_object({}) do |rental_data, hash|
+  car = cars[rental_data[:car_id]]
+  rental = Rental.new(**rental_data.except(:car_id).merge(car:))
+  hash[rental.id] = rental
+end
+
+# Output
+output = { rentals: rentals.values.map { |rental| {id: rental.id, price: rental.price, commission: rental.commission.to_h }} }
+
+File.write("data/output.json", JSON.pretty_generate(output))
+puts "✅ Output written to output.json"
---
+++ level3/spec/rental_spec.rb
@@ -0,0 +1,28 @@
+RSpec.describe Rental do
+  let(:car) { Car.new(id: 1, price_per_day: 2000, price_per_km: 10) }
+
+  it "calculates the duration correctly" do
+    rental = Rental.new(id: 1, car:, start_date: "2017-12-8", end_date: "2017-12-10", distance: 100)
+    expect(rental.duration).to eq(3)
+  end
+
+  it "calculates the price correctly when no discount can be applied" do
+    rental = Rental.new(id: 1, car:, start_date: "2017-12-8", end_date: "2017-12-8", distance: 100)
+    expect(rental.price).to eq(3000) # [(1 * 2000)] + 100 * 10
+  end
+
+  it "calculates the price correctly when all discounts are applied" do
+    rental = Rental.new(id: 1, car:, start_date: "2017-12-8", end_date: "2017-12-27", distance: 100)
+    expect(rental.price).to eq(26800) # [(1 * 2000) + (3 * 2000 * (1 - 0.1)) + (6 * 2000 * (1 - 0.3)) + (10 * 2000 * (1 - 0.5))] + 100 * 10
+  end
+
+  it "calculates the price correctly" do
+    rental = Rental.new(id: 1, car:, start_date: "2017-12-8", end_date: "2017-12-10", distance: 100)
+    expect(rental.price).to eq(6600) # [(1 * 2000) + (2 * 2000 * (1 - 0.10))] + 100 * 10
+  end
+
+  it "calculates the price correctly" do
+    rental = Rental.new(id: 1, car:, start_date: "2017-12-8", end_date: "2017-12-10", distance: 100)
+    expect(rental.price).to eq(6600) # [(1 * 2000) + (2 * 2000 * (1 - 0.10))] + 100 * 10
+  end
+end
